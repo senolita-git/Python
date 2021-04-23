88382c3c05133b3a769adc1c9db42551e6729232
senolita <senanrymnn@hotmail.com>
2021-04-24 00:25:17 +0300

    python-password-reminder

---
+++ python1.py
@@ -0,0 +1,7 @@
+#passwordreminder
+name = input("Please enter your name: ")
+name = name.title()
+if name == "Joseph":
+  print("Hello Joseph! The password is : W@12")
+else :
+    print("Hello",name+"!", "See you later.")
