7d2046d6b4a32d9373e4f42a913f5ec9c48c1072
senolita <senanrymnn@hotmail.com>
2021-05-08 13:03:22 +0300

    prime-number

--- python-assignment-3.py
+++ python-assignment-3.py
@@ -1,17 +1,14 @@
 number = input("Please enter a number: ")
-list = [2,3,4,5,6,7,8,9]
 count = 0
-if int(number) != 2 and int(number) != 5 and int(number) != 7 :
-  for i in list:
-    if int(number) != 1 and not int(number)%int(i) == 0:
-      count +=1
-      if count == 8:
-        print("Number is a prime number")
-    else: 
-      print("Number is not a prime number")
-      break
+for i in range(1,int(number)+1):
+  if int(number)%i == 0:
+    count += 1
+if count >= 3 or int(number) == 1 or int(number) == 0:
+      print("The number is not a prime number")
 else:
-  print("Number is a prime number")
+      print("The number is a prime number")
+      
+  
 
 
 
